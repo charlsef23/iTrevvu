5f8ba10af1154fdd78142d0c6f788ab17c9dd4ee
Charlsef23 <charlsef66@gmail.com>
2025-12-30 09:16:33 +0100

    Initial Commit

---
+++ iTrevvuTests/iTrevvuTests.swift
@@ -0,0 +1,17 @@
+//
+//  iTrevvuTests.swift
+//  iTrevvuTests
+//
+//  Created by Carlos Esteve Fernández on 30/12/25.
+//
+
+import Testing
+@testable import iTrevvu
+
+struct iTrevvuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
